d7392a73fd1b03f8b73b1813dc3989fbbf347018
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-11-14 20:41:32 +0100

    Changed the cargo and the genotype structure.

--- src/structures/genotype.rs
+++ src/structures/genotype.rs
@@ -1,15 +1,27 @@
 use crate::structures::gene::Gene;
 
 pub struct Genotype<T: Gene>{
-    pub dna: Vec<T>
+    pub dna: Vec<T>,
+    pub phenotype: f64,
 }
+
 impl<T: Gene> Genotype<T>{
+    
     pub fn new() -> Genotype<T>{
         Genotype{
             dna: Vec::new(),
+            phenotype: 0.0,
         }
     }
-    pub fn phenotype(&self) -> f64{
-        return 0.0
+
+    /**
+     * Calculates the phenotype, adding the values of the dna
+     */
+    fn calculate_phenotype(&self) -> f64{
+        let mut phenotype = 0.0;
+        for gene in &self.dna {
+            phenotype += gene.value();
+        }
+        phenotype
     }
 }
