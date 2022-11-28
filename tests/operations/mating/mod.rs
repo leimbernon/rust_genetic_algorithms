87fb097011f9934e4ace4fcaa765188d3d20eb8e
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-11-28 18:56:00 +0100

    Modified the structure of the tests, to give in the same place all the structures, and added the swap mutation unit test.

--- tests/operations/mating/mod.rs
+++ tests/operations/mating/mod.rs
@@ -1,39 +1,5 @@
-use genetic_algorithms::traits::{GeneT, GenotypeT};
+use crate::structures::{Gene, Genotype};
 use genetic_algorithms::operations::{mating::random};
-
-//Structures definition
-#[derive(Debug, Copy, Clone, Default)]
-struct Gene{
-    pub id: i64,
-}
-impl GeneT for Gene{
-    fn new()->Gene{
-        return Gene{id: -1};
-    }
-    fn get_id(&self) -> &i64{
-        return &self.id;
-    }
-}
-
-#[derive(Debug)]
-pub struct Genotype<T: GeneT>{
-    pub dna: Vec<T>,
-    pub phenotype: f64,
-}
-impl <T: GeneT> GenotypeT<T> for Genotype<T>{
-    fn get_dna(&mut self) -> &mut Vec<T> {
-        &mut self.dna
-    }
-    fn get_phenotype(&self) -> &f64 {
-        return &self.phenotype;
-    }
-    fn new() -> Self {
-       return Genotype{
-        dna: Vec::new(),
-        phenotype: 0.0,
-       }
-    }
-}
 
 #[test]
 fn test_random_even_mating(){
