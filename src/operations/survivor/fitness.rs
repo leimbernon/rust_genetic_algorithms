ba3a3d7d825953c93c38b41ba95400ff97ebfc6e
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-12-01 18:39:31 +0100

    Added the maximization or minimization function in problem solving. Also added the unit tests.

--- src/operations/survivor/fitness.rs
+++ src/operations/survivor/fitness.rs
@@ -1,6 +1,12 @@
 use crate::traits::{GeneT, GenotypeT};
 
-pub fn fitness_based<T:GeneT, U:GenotypeT<T>>(mut individuals: Vec<U>, population_size: usize) -> Vec<U>{
+pub enum ProblemSolving {
+    Minimization,
+    Maximization,
+}
+
+
+pub fn fitness_based<T:GeneT, U:GenotypeT<T>>(mut individuals: Vec<U>, population_size: usize, problem_solving: ProblemSolving) -> Vec<U>{
 
     //We first sort the individuals by their fitness
     individuals.sort_by(|a, b| b.get_phenotype().partial_cmp(&a.get_phenotype()).unwrap());
@@ -8,9 +14,19 @@ pub fn fitness_based<T:GeneT, U:GenotypeT<T>>(mut individuals: Vec<U>, populatio
     //If there is more individuals than the defined population number
     if individuals.len() > population_size {
         let individuals_to_remove = individuals.len() - population_size;
-        for _i in 0..individuals_to_remove{
-            individuals.remove(individuals.len() - 1);
-        }        
+
+        match problem_solving {
+            ProblemSolving::Minimization => {
+                for _i in 0..individuals_to_remove{
+                    individuals.remove(individuals.len() - 1);
+                }
+            },
+            ProblemSolving::Maximization => {
+                for _i in 0..individuals_to_remove{
+                    individuals.remove(0);
+                }
+            },
+        }
     }
 
     return individuals;
