7b012c03cd18ff422a577ea95135659e7f3ac1de
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-12-03 14:25:16 +0100

    Fixed some problems with the types.

--- src/ga.rs
+++ src/ga.rs
@@ -1,4 +1,4 @@
-use crate::{population::{Population}, traits::{GenotypeT, GeneT}, operations::{Crossover, Selection, Mutation, Survivor, selection}};
+use crate::{population::{Population}, traits::{GenotypeT, GeneT}, operations::{Crossover, Selection, Mutation, Survivor, selection, crossover}};
 
 #[derive(Copy, Clone)]
 pub enum ProblemSolving {
@@ -19,15 +19,24 @@ pub struct GaConfiguration {
 /**
  * Function to start the genetic algorithms cycle
  */
-pub fn start<T:GeneT, U:GenotypeT<T>>(population: Population<T,U>, configuration: GaConfiguration)->Vec<U>{
-
+pub fn start<T:GeneT, U:GenotypeT<T> + Copy>(population: Population<T,U>, configuration: GaConfiguration)->Vec<U>
+{
     let initial_population_size = population.size();
 
     //We start the cycles
     for i in 0..configuration.max_generations {
 
-        //1- Parent selection
-        selection::factory(configuration.selection, population.individuals());
+        //1- Parent selection for reproduction
+        let parents = selection::factory(configuration.selection, &population.individuals());
+
+        //2- Reproduce the parents
+        for j in 0..parents.len() {
+            
+            let mut parent_1 = population.individuals().get(0).copied().unwrap();
+            let mut parent_2 = population.individuals().get(1).copied().unwrap();
+
+            let offspring = crossover::factory(configuration.crossover, &mut parent_1, &mut parent_2);
+        }
 
     }
 
