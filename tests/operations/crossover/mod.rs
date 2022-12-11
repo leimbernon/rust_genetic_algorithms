0206a5d81ffbc6bee5326706a0c2fc4b7c162aa4
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-12-11 13:10:02 +0100

    test(multipoint-crossover-4-points): added a new unit test

--- tests/operations/crossover/mod.rs
+++ tests/operations/crossover/mod.rs
@@ -77,3 +77,42 @@ fn test_multipoint_crossover_2_points(){
     assert_eq!(child_2.dna.get(4).unwrap().id, 2);
     assert_eq!(child_2.dna.get(5).unwrap().id, 1);
 }
+
+#[test]
+fn test_multipoint_crossover_4_points(){
+
+    //we create 2 dnas of 4 genes for 2 individuals
+    let dna_1 = vec![Gene{id:1}, Gene{id:2}, Gene{id:3}, Gene{id:4}, Gene{id:5}, Gene{id:6}];
+    let dna_2 = vec![Gene{id:6}, Gene{id:5}, Gene{id:4}, Gene{id:3}, Gene{id:2}, Gene{id:1}];
+
+    let mut parent_1 = Genotype{dna: dna_1, phenotype: 0.0};
+    let mut parent_2 = Genotype{dna: dna_2, phenotype: 0.0};
+
+    //Getting the offspring
+    let mut offspring = multipoint_crossover(&mut parent_1, &mut parent_2, &4).unwrap();
+
+    //Setting the child
+    let child_2 = offspring.pop().unwrap();
+    let child_1 = offspring.pop().unwrap();
+
+    //Checking that children have the same number of genes
+    assert_eq!(child_1.dna.len(), parent_1.dna.len());
+    assert_eq!(child_2.dna.len(), child_2.dna.len());
+    assert_eq!(parent_1.dna.len(), parent_2.dna.len());
+
+    //Checking that the crossover has been well executed for the child 1
+    assert_eq!(child_1.dna.get(0).unwrap().id, 1);
+    assert_eq!(child_1.dna.get(1).unwrap().id, 5);
+    assert_eq!(child_1.dna.get(2).unwrap().id, 3);
+    assert_eq!(child_1.dna.get(3).unwrap().id, 3);
+    assert_eq!(child_1.dna.get(4).unwrap().id, 5);
+    assert_eq!(child_1.dna.get(5).unwrap().id, 1);
+
+    //Checking that the crossover has been well executed for the child 2
+    assert_eq!(child_2.dna.get(0).unwrap().id, 6);
+    assert_eq!(child_2.dna.get(1).unwrap().id, 2);
+    assert_eq!(child_2.dna.get(2).unwrap().id, 4);
+    assert_eq!(child_2.dna.get(3).unwrap().id, 4);
+    assert_eq!(child_2.dna.get(4).unwrap().id, 2);
+    assert_eq!(child_2.dna.get(5).unwrap().id, 6);
+}
