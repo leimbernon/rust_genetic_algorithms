6e2dc644b6e58dbdc157619313dbfb116e720a49
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-12-22 16:22:52 +0100

    feat(selection-method): selection-method
    Added the stochastic universal sampling

--- src/operations/selection/fitness_proportionate.rs
+++ src/operations/selection/fitness_proportionate.rs
@@ -38,3 +38,67 @@ pub fn roulette_wheel_selection<T:GeneT, U:GenotypeT<T>>(individuals: &Vec<U>) -
 
     return mating;
 }
+
+
+pub fn stochastic_universal_sampling<T:GeneT, U:GenotypeT<T>>(individuals: &Vec<U>, couples: i32) -> HashMap<usize, usize>{
+    
+    let mut mating = HashMap::new();
+    let individual_couples = (couples*2) as usize;
+
+    //1- Calculate the selection probabilities
+    let mut selection_probabilities: Vec<usize> = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for genotype in individuals 
+    { 
+        let selection_probability = (*genotype.get_phenotype() as usize) / individuals.len();
+        selection_probabilities.push(selection_probability);
+    }
+
+    //2- Calculate the pointer distance and the starting point between 0 and the pointer distance
+    let pointer_distance = (1 / (individual_couples)) as f64;
+    let starting_point = rng.gen_range(0.0..pointer_distance);
+
+    //3- Parent identification
+    let mut current_point = starting_point;
+    let mut next_individual = 1;
+
+    let mut end_of_individuals = false;
+    let mut couple_completed = false;
+    let mut first_mate = 0;
+
+    for i in 0..individual_couples{
+       
+        //We check that there are enough individuals
+        if i >= individuals.len(){
+            break;
+        }else if next_individual >= individuals.len(){
+            end_of_individuals = true;
+        }
+
+        //We check if the pointer is between the current and the next individual
+        if !end_of_individuals && current_point >= selection_probabilities[i] as f64 && 
+            current_point < selection_probabilities[next_individual] as f64 {
+
+            if couple_completed {
+                mating.insert(first_mate, i);
+                couple_completed = !couple_completed;
+            }else{
+                first_mate = i;
+            }
+
+        } else if end_of_individuals && current_point >= selection_probabilities[i] as f64 {
+            if couple_completed {
+                mating.insert(first_mate, i);
+                couple_completed = !couple_completed;
+            }else{
+                first_mate = i;
+            }
+        }
+
+        current_point = current_point + pointer_distance;
+        next_individual += 1;
+    }
+
+    return mating;
+}
