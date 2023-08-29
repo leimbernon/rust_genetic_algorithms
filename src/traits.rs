5260a6e90689dfffdc4d2e34ca389f5db64be06e
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-08-29 16:34:50 +0200

    Added new function to set a gene in a defined DNA position.

--- src/traits.rs
+++ src/traits.rs
@@ -12,6 +12,11 @@ pub trait GenotypeT: Clone{
     fn get_dna(&self) -> &[Self::Gene];
     fn get_dna_mut(&mut self) -> &mut Vec<Self::Gene>;
     fn set_dna(&mut self, dna: &[Self::Gene]);
+    fn set_gene(&mut self, gene_index: usize, gene: Self::Gene){
+        let mut dna_temp = self.get_dna().to_vec();
+        dna_temp[gene_index] = gene;
+        self.set_dna(dna_temp.as_slice());
+    }
     fn calculate_fitness(&mut self);
     fn get_fitness(&self) -> f64;
     fn set_fitness(&mut self, fitness: f64);
