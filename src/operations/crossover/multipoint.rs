c1f0603c808477d3d3075b9f249f5795812b834c
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-09-03 12:07:57 +0200

    Added log traces in the crossover methods.

--- src/operations/crossover/multipoint.rs
+++ src/operations/crossover/multipoint.rs
@@ -1,4 +1,5 @@
 use crate::traits::GenotypeT;
+use log::{trace, debug};
 
 pub fn multipoint_crossover<U: GenotypeT>(parent_1: &U, parent_2: &U, crossover_number_of_points: &i32) -> Option<Vec<U>>{
 
@@ -12,14 +13,17 @@ pub fn multipoint_crossover<U: GenotypeT>(parent_1: &U, parent_2: &U, crossover_
 
     let mut dna_child_1 = Vec::new();
     let mut dna_child_2 = Vec::new();
+    debug!(target="crossover_events", method="multipoint_crossover"; "Starting the  multipoint crossover");
 
     //We check if the number of points are higher than the dna, we take the dna lenght
     let number_of_blocks = if (*crossover_number_of_points as usize) + 1 > parent_1.get_dna().len(){parent_1.get_dna().len()}else{(*crossover_number_of_points as usize) + 1};
+    trace!(target="crossover_events", method="multipoint_crossover"; "Number of blocks {}", number_of_blocks);
 
     //We get the number of genes per block
     let number_of_genes_per_block = (parent_1.get_dna().len() / number_of_blocks) as i64;
     let mut gene_number = 0;
     let mut crossed = false;
+    trace!(target="crossover_events", method="multipoint_crossover"; "Number of genes per block {}", number_of_genes_per_block);
 
     //Here we set the genes to the children
     for gn in 0..parent_1.get_dna().len(){
@@ -45,6 +49,7 @@ pub fn multipoint_crossover<U: GenotypeT>(parent_1: &U, parent_2: &U, crossover_
     //Sets the dna into the children and return them
     child_1.set_dna(dna_child_1.as_slice());
     child_2.set_dna(dna_child_2.as_slice());
+    debug!(target="crossover_events", method="multipoint_crossover"; "Multipoint crossover finished");
 
     Some(vec![child_1, child_2])
 }
