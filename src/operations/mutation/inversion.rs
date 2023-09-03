9dd5f124588b72ee46633436586cf22e82dd4782
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-09-03 12:16:38 +0200

    Added log traces for mutation methods.

--- src/operations/mutation/inversion.rs
+++ src/operations/mutation/inversion.rs
@@ -1,16 +1,18 @@
 use std::cmp::Ordering;
-
 pub(crate) use crate::traits::GenotypeT;
 use rand::Rng;
+use log::{trace, debug};
 
 pub fn inversion<U: GenotypeT>(individual: &mut U){
 
     //Getting two random genes from the dna of the individual
+    debug!(target="mutation_events", method="inversion"; "Starting the inversion mutation");
     let mut rng = rand::thread_rng();
     let index_1: usize = rng.gen_range(0..individual.get_dna().len());
     let index_2: usize = rng.gen_range(0..individual.get_dna().len());
     let mut lower_index: usize = 0;
     let mut higher_index: usize = 0;
+    trace!(target="mutation_events", method="inversion"; "Mutation index 1: {}, mutation index 2: {}", index_1, index_2);
 
     //We create the indexes that we want extract
     match index_1.cmp(&index_2) {
@@ -36,4 +38,6 @@ pub fn inversion<U: GenotypeT>(individual: &mut U){
         individual.set_gene(lower_index + i, gene_end);
         individual.set_gene(higher_index - i, gene_start);
     }
+
+    debug!(target="mutation_events", method="inversion"; "Inversion mutation finished");
 }
--- src/operations/mutation/swap.rs
+++ src/operations/mutation/swap.rs
@@ -1,12 +1,15 @@
 pub(crate) use rand::Rng;
 use crate::traits::GenotypeT;
+use log::{trace, debug};
 
 pub fn swap<U: GenotypeT>(individual: &mut U){
 
     //Getting two random genes from the dna of the individual
+    debug!(target="mutation_events", method="swap"; "Starting the swap mutation");
     let mut rng = rand::thread_rng();
     let index_1 = rng.gen_range(0..individual.get_dna().len());
     let index_2 = rng.gen_range(0..individual.get_dna().len());
+    trace!(target="mutation_events", method="swap"; "Mutation index 1: {}, mutation index 2: {}", index_1, index_2);
 
     let gene_1 = individual.get_dna().get(index_1).copied().unwrap();
     let gene_2 = individual.get_dna().get(index_2).copied().unwrap();
@@ -14,4 +17,5 @@ pub fn swap<U: GenotypeT>(individual: &mut U){
     //Swapping both genes
     individual.set_gene(index_1, gene_2);
     individual.set_gene(index_2, gene_1);
+    debug!(target="mutation_events", method="swap"; "Swap mutation finished");
 }
