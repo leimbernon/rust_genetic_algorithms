5dd9c1885cfac636d5f04b9f4cf649463c64b1ff
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-09-25 20:27:40 +0200

    Added the function to calculate the maximum fitness of the population without going through the entire population. This way, we save many loops.

--- src/population.rs
+++ src/population.rs
@@ -12,6 +12,9 @@ where
 
     //Average fitness of the population
     pub f_avg: f64,
+
+    //Population largest fitness value
+    pub f_max: f64,
 }
 
 impl<U> Population<U>
@@ -20,21 +23,27 @@ where
 {
     // Creates a new empty `Population`
     pub fn new_empty() -> Population<U> {
-        Population { individuals: vec![], generation_numbers: vec![], f_avg: 0.0 }
+        Population { individuals: vec![], generation_numbers: vec![], f_avg: 0.0, f_max: 0.0 }
     }
 
     // Creates a new `Population` with the given individuals as members.
     pub fn new(individuals: Vec<U>) -> Population<U> {
-        Population { individuals, generation_numbers: vec![], f_avg: 0.0}
+        Population { individuals, generation_numbers: vec![], f_avg: 0.0, f_max: 0.0}
     }
 
     // Adds an individual with a generation number.
     pub fn add_individual_gn(&mut self, individual: U, generation_number: i32){
+        
+        //We calculate the f_max
+        self.f_max = if individual.get_fitness() > self.f_max {individual.get_fitness()} else{self.f_max};
+
+        //We recalculate the average fitness and add the individual
         self.f_avg *= self.individuals.len() as f64;
         self.f_avg += individual.get_fitness();
         self.individuals.push(individual);
         self.generation_numbers.push(generation_number);
         self.f_avg /= self.individuals.len() as f64;
+
     }
 
     //Function to add individuals in the list and recalculate the fitness without going through the entire population
@@ -42,6 +51,8 @@ where
         self.f_avg *= self.individuals.len() as f64;
         self.individuals.append(individuals);
         for individual in individuals{
+            //We calculate the f_max and add the values to the f_avg
+            self.f_max = if individual.get_fitness() > self.f_max {individual.get_fitness()} else{self.f_max};
             self.f_avg += individual.get_fitness();
         }
         self.f_avg /= self.individuals.len() as f64;
