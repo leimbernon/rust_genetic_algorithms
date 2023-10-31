33f6832e0a97ffbdade15d43f3a27812945726c2
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-10-31 17:55:27 +0100

    Added the trait for the configuration of the genetic algorithms

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,5 @@
+use crate::{configuration::{LogLevel, ProblemSolving}, operations::{Survivor, Selection, Crossover, Mutation}};
+
 pub trait GeneT: Default + Clone + Copy + Sync + Send {
     fn new() -> Self;
     fn get_id(&self) -> i32{0}
@@ -28,4 +30,34 @@ pub trait GenotypeT: Clone{
     fn get_fitness_distance(&self, fitness_target: &f64) -> f64 {
         (fitness_target - self.get_fitness()).abs()
     }
+}
+
+pub trait Configuration{
+    fn new() -> Self;
+    fn with_adaptive_ga(self, adaptive_ga: bool) -> Self;
+    fn with_threads(self, number_of_threads: i32)-> Self;
+    fn with_logs(self, log_level: LogLevel) -> Self;
+    fn with_survivor_method(self, method: Survivor) -> Self;
+
+    //Limit configuration
+    fn with_problem_solving(self, problem_solving: ProblemSolving)-> Self;
+    fn with_max_generations(self, max_generations: i32)-> Self;
+    fn with_fitness_target(self, fitness_target: f64)-> Self;
+    fn with_best_individual_by_generation(self, best_individual_by_generation: bool) -> Self;
+
+    //Selection configuration
+    fn with_number_of_couples(self, number_of_couples: i32)->Self;
+    fn with_selection_method(self, selection_method: Selection)->Self;
+
+    //Crossover configuration
+    fn with_crossover_number_of_points(self, number_of_points: i32)->Self;
+    fn with_crossover_probability_max(self, probability_max: f64)->Self;
+    fn with_crossover_probability_min(self, probability_min: f64) -> Self;
+    fn with_crossover_method(self, method: Crossover) -> Self;
+
+    //Mutation configuration
+    fn with_mutation_probability_max(self, probability_max: f64)->Self;
+    fn with_mutation_probability_min(self, probability_min: f64) -> Self;
+    fn with_mutation_method(self, method: Mutation) -> Self;
+
 }
